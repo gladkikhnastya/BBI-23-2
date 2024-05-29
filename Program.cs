d29ed27e31bde1ce7e3a0f10edb28c484b7916b5
gladkikhnastya <160515563+gladkikhnastya@users.noreply.github.com>
2024-05-29 21:49:06 +0300

    8 конец.

--- Program.cs
+++ Program.cs
@@ -1,149 +1,153 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 
-//1.2
-using System;
-
-abstract class Running
+abstract class Task
 {
-    protected string name;
-    protected double time;
-
-    public Running(string name, double time)
-    {
-        this.name = name;
-        this.time = time;
-    }
-
-    public abstract void DisplayInfo();
+    public abstract string Process(string input);
 }
 
-class Running100m : Running
+class Encryptor : Task
 {
-    public Running100m(string name, double time) : base(name, time) { }
-
-    public override void DisplayInfo()
+    public override string Process(string input)
     {
-        Console.WriteLine($"Name: {name}, Distance: 100m, Time: {time} seconds");
-    }
-}
+        string[] words = input.Split(' ');
+        StringBuilder encryptedMessage = new StringBuilder();
 
-class Running500m : Running
-{
-    public Running500m(string name, double time) : base(name, time) { }
-
-    public override void DisplayInfo()
-    {
-        Console.WriteLine($"Name: {name}, Distance: 500m, Time: {time} seconds");
-    }
-}
-
-struct Sportsmen
-{
-    private string name;
-    private string surname;
-    private string group;
-    private string tutor;
-    private double result;
-
-    public Sportsmen(string name, string surname, string group, string tutor, double result)
-    {
-        this.name = name;
-        this.surname = surname;
-        this.group = group;
-        this.tutor = tutor;
-        this.result = result;
-    }
-
-    public void DisplayInfo()
-    {
-        Console.WriteLine($"Имя: {name}, Фамилия: {surname}, Группа: {group}, Тренер: {tutor}, Результат: {result}");
-    }
-
-    public double GetResult()
-    {
-        return result;
-    }
-}
-
-class Program
-{
-    static void Main()
-    {
-        Running[] runners = new Running[]
+        foreach (string word in words)
         {
-            new Running100m("Иванова Катя", 12.5),
-            new Running100m("Петрова Маша", 11.8),
-            new Running100m("Сидорова Лера", 13.2),
-            new Running500m("Кузнецова Соня", 60.3),
-            new Running500m("Макарова Валя", 58.9)
-        };
+            char[] characters = word.ToCharArray();
+            Array.Reverse(characters);
+            encryptedMessage.Append(new string(characters) + " ");
+        }
 
-        Console.WriteLine("Results for 100m race:");
-        Console.WriteLine("-----------------------");
-        foreach (var runner in runners)
+        return encryptedMessage.ToString().Trim();
+    }
+}
+
+class Decryptor : Task
+{
+    public override string Process(string input)
+    {
+        string[] words = input.Split(' ');
+        StringBuilder decryptedMessage = new StringBuilder();
+
+        foreach (string word in words)
         {
-            if (runner is Running100m)
+            char[] characters = word.ToCharArray();
+            Array.Reverse(characters);
+            decryptedMessage.Append(new string(characters) + " ");
+        }
+
+        return decryptedMessage.ToString().Trim();
+    }
+}
+
+class WordPunctuationAnalyzer : Task
+{
+    public override string Process(string input)
+    {
+        string[] wordsAndPunctuation = Regex.Split(input, @"(\W)");
+        int wordCount = 0;
+        int punctuationCount = 0;
+
+        foreach (string item in wordsAndPunctuation)
+        {
+            if (!string.IsNullOrWhiteSpace(item))
             {
-                runner.DisplayInfo();
+                if (char.IsLetterOrDigit(item[0]))
+                    wordCount++;
+                else
+                    punctuationCount++;
             }
         }
 
-        Console.WriteLine("\nResults for 500m race:");
-        Console.WriteLine("-----------------------");
-        foreach (var runner in runners)
+        return $"Сложность предложения: {wordCount + punctuationCount}";
+    }
+}
+
+class LetterFrequencyAnalyzer : Task
+{
+    public override string Process(string input)
+    {
+        Dictionary<char, int> letterFrequencies = new Dictionary<char, int>();
+
+        string[] words = input.Split(new[] { ' ', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (string word in words)
         {
-            if (runner is Running500m)
+            if (!string.IsNullOrEmpty(word))
             {
-                runner.DisplayInfo();
+                char firstLetter = word[0];
+
+                if (char.IsLetter(firstLetter))
+                {
+                    if (letterFrequencies.ContainsKey(firstLetter))
+                        letterFrequencies[firstLetter]++;
+                    else
+                        letterFrequencies[firstLetter] = 1;
+                }
             }
         }
-    }
-}
 
-//2.1
-using System;
+        var sortedFrequencies = letterFrequencies.OrderByDescending(pair => pair.Value);
 
-class Person
-{
-    protected string firstName;
-    protected string lastName;
-    protected string patronymic;
-
-    public Person(string firstName, string lastName, string patronymic)
-    {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.patronymic = patronymic;
-    }
-
-    public string GetFullName()
-    {
-        return $"{lastName} {firstName} {patronymic}";
-    }
-}
-
-class Student : Person
-{
-    private int studentId;
-    private double[] grades;
-    private double averageGrade;
-
-    public Student(string firstName, string lastName, string patronymic, int studentId, double[] grades) : base(firstName, lastName, patronymic)
-    {
-        this.studentId = studentId;
-        this.grades = grades;
-        CalculateAverageGrade();
-        Console.WriteLine($"ФИО: {GetFullName()}, № студ. билета: {studentId}, Средний балл: {averageGrade}");
-    }
-
-    private void CalculateAverageGrade()
-    {
-        double sum = 0;
-        foreach (double grade in grades)
+        StringBuilder result = new StringBuilder();
+        result.AppendLine("Буквы, на которые начинаются слова в тексте, в порядке убывания частоты их употребления:");
+        foreach (var pair in sortedFrequencies)
         {
-            sum += grade;
+            result.AppendLine($"{pair.Key}: {pair.Value}");
         }
-        averageGrade = sum / grades.Length;
+
+        return result.ToString();
+    }
+}
+
+class SequenceAnalyzer : Task
+{
+    public override string Process(string input)
+    {
+        Console.WriteLine("Введите последовательность букв:");
+        string sequence = Console.ReadLine();
+
+        List<string> wordsContainingSequence = new List<string>();
+        string[] words = Regex.Split(input, @"\W+");
+
+        foreach (string word in words)
+        {
+            if (word.Contains(sequence, StringComparison.OrdinalIgnoreCase))
+                wordsContainingSequence.Add(word);
+        }
+
+        return $"Слова, содержащие последовательность '{sequence}': {string.Join(", ", wordsContainingSequence)}";
+    }
+}
+
+class SurnameSortingTask : Task
+{
+    public override string Process(string input)
+    {
+        string[] surnames = input.Split(',').Select(s => s.Trim()).OrderBy(s => s).ToArray();
+        return string.Join(", ", surnames);
+    }
+}
+
+class NumberSumTask : Task
+{
+    public override string Process(string input)
+    {
+        int sum = 0;
+        string[] words = input.Split(new[] { ' ', ',', '.', '!', '?', ':', ';', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (string word in words)
+        {
+            if (int.TryParse(word, out int number) && number >= 1 && number <= 10)
+                sum += number;
+        }
+
+        return $"Сумма чисел от 1 до 10 в тексте: {sum}";
     }
 }
 
@@ -151,129 +155,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Student[] students = new Student[3];
-        students[0] = new Student("Иван", "Наумов", "Петрович", 12345, new double[] { 5.0, 5.0, 5.0, 5.0, 5.0 });
-        students[1] = new Student("Петр", "Селиванов", "Иванович", 54321, new double[] { 2.0, 2.0, 3.0, 4.0, 4.0 });
-        students[2] = new Student("Алексей", "Аборин", "Сергеевич", 98765, new double[] { 5.0, 4.0, 4.0, 4.0, 4.0 });
+        Console.WriteLine("Выберите задание: ");
+        Console.WriteLine("1) Зашифровать сообщение.");
+        Console.WriteLine("2) Расшифровать сообщение.");
+        Console.WriteLine("3) Определить сложность предложения.");
+        Console.WriteLine("4) Вывести буквы, на которые начинаются слова в тексте, в порядке убывания частоты их употребления.");
+        Console.WriteLine("5) Вывести слова, содержащие заданную последовательность букв.");
+        Console.WriteLine("6) Упорядочить список фамилий по алфавиту.");
+        Console.WriteLine("7) Найти сумму чисел от 1 до 10 в тексте.");
+        Console.Write("Ваш выбор: ");
 
-        Console.WriteLine();
+        int choice = int.Parse(Console.ReadLine());
 
-        for (int i = 0; i < students.Length - 1; i++)
+        Task task;
+        switch (choice)
         {
-            double max = students[i].averageGrade;
-            int index = i;
-            for (int j = i + 1; j < students.Length; j++)
-            {
-                if (students[j].averageGrade > max)
-                {
-                    max = students[j].averageGrade;
-                    index = j;
-                }
-
-                Student temp = students[index];
-                students[index] = students[i];
-                students[i] = temp;
-            }
+            case 1:
+                task = new Encryptor();
+                break;
+            case 2:
+                task = new Decryptor();
+                break;
+            case 3:
+                task = new WordPunctuationAnalyzer();
+                break;
+            case 4:
+                task = new LetterFrequencyAnalyzer();
+                break;
+            case 5:
+                task = new SequenceAnalyzer();
+                break;
+            case 6:
+                task = new SurnameSortingTask();
+                break;
+            case 7:
+                task = new NumberSumTask();
+                break;
+            default:
+                Console.WriteLine("Недопустимый выбор.");
+                return;
         }
 
-        Console.WriteLine();
-        for (int i = 0; i < students.Length; i++)
-        {
-            if (students[i].averageGrade >= 4)
-            {
-                Console.WriteLine($"ФИО: {students[i].GetFullName()}, № студ. билета: {students[i].studentId}, Средний балл: {students[i].averageGrade}");
-            }
-        }
+        Console.Write("Введите текст: ");
+        string input = Console.ReadLine();
+
+        string result = task.Process(input);
+        Console.WriteLine($"Результат:\n{result}");
     }
 }
-
-
-
-
-//3.1
-using System;
-
-class Group
-{
-    public string Name { get; private set; }
-    protected int[] examScores;
-    protected string[] subjects;
-
-    public Group(string name, int[] scores, string[] subjects)
-    {
-        Name = name;
-        examScores = scores;
-        this.subjects = subjects;
-    }
-
-    public virtual double GetAverageScore()
-    {
-        int sum = 0;
-        foreach (int score in examScores)
-        {
-            sum += score;
-        }
-        return (double)sum / examScores.Length;
-    }
-
-    public void PrintStudents()
-    {
-        Console.WriteLine($"Group: {Name}");
-        for (int i = 0; i < examScores.Length; i++)
-        {
-            Console.WriteLine($"Student {i + 1}: Average Score - {GetAverageScore()} | Subjects: {string.Join(", ", subjects)}");
-        }
-        Console.WriteLine();
-    }
-}
-
-class GroupA : Group
-{
-    public GroupA(string name, int[] scores) : base(name, scores, new string[] { "Math", "Physics" }) { }
-
-    public override double GetAverageScore()
-    {
-        // Custom calculation for GroupA average score
-        return base.GetAverageScore() * 1.1; // Adding 10%
-    }
-}
-
-class GroupB : Group
-{
-    public GroupB(string name, int[] scores) : base(name, scores, new string[] { "English", "History" }) { }
-
-    public override double GetAverageScore()
-    {
-        // Custom calculation for GroupB average score
-        return base.GetAverageScore() * 0.9; // Subtracting 10%
-    }
-}
-
-class GroupC : Group
-{
-    public GroupC(string name, int[] scores) : base(name, scores, new string[] { "Chemistry", "Biology" }) { }
-
-    public override double GetAverageScore()
-    {
-        // Custom calculation for GroupC average score
-        return base.GetAverageScore(); // No change
-    }
-}
-
-class Program
-{
-    static void Main()
-    {
-        Group[] groups = new Group[3];
-        groups[0] = new GroupA("1", new int[] { 5, 5, 5, 5, 5 });
-        groups[1] = new GroupB("2", new int[] { 2, 2, 3, 4, 4 });
-        groups[2] = new GroupC("3", new int[] { 5, 4, 4, 4, 4 });
-
-        Console.WriteLine("Students from all groups:");
-        foreach (var group in groups)
-        {
-            group.PrintStudents();
-        }
-    }
-}
-
